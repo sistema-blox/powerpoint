da666d6471f9a6a5021154485e0dc1905eff2584
Raniery <raniery@saleszera.com>
2024-09-19 17:36:36 -0300

    test: add dependencies to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,5 @@
 RSpec.configure do |_|
   require "support/shoulda_matchers"
   require "support/fixture_file_path"
+  require "support/dependencies"
 end
---
+++ spec/support/dependencies.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "powerpoint/version"
+require "powerpoint/util"
+require "powerpoint/slide/intro"
+require "powerpoint/slide/textual"
+require "powerpoint/slide/pictorial"
+require "powerpoint/slide/text_picture_split"
+require "powerpoint/slide/picture_description"
+require "powerpoint/compression"
+require "powerpoint/presentation"
+require "pry-byebug"
