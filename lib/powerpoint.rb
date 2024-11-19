607bec8be5976521e22a6c8fbf04aada2c4bd9c4
Raniery <raniery@saleszera.com>
2024-11-19 11:13:36 -0300

    feat: add a support to custom configs

--- lib/powerpoint.rb
+++ lib/powerpoint.rb
@@ -9,9 +9,29 @@ require "powerpoint/slide/text_picture_split"
 require "powerpoint/slide/picture_description"
 require "powerpoint/compression"
 require "powerpoint/presentation"
+require "powerpoint/configuration"
+require "powerpoint/google_services/api"
+require "powerpoint/google_services/base"
+require "powerpoint/google_services/template"
 
 module Powerpoint
   ROOT_PATH = File.expand_path("../..", __FILE__)
   TEMPLATE_PATH = "#{ROOT_PATH}/template/"
   VIEW_PATH = "#{ROOT_PATH}/lib/powerpoint/views/"
+
+  class << self
+    attr_accessor :configuration, :google
+  end
+
+  def self.configure
+    self.configuration ||= Powerpoint::Configuration.new
+
+    yield(configuration) if block_given?
+
+    self.google ||= Powerpoint::GoogleServices::Api.new
+  end
+
+  def self.provider
+    configuration.nil? ? :local : :google
+  end
 end
---
+++ lib/powerpoint/configuration.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Powerpoint
+  class Configuration
+    attr_accessor :google_credentials_path
+  end
+end
